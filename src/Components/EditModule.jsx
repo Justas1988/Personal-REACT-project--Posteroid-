76350ec149a9671446579ade82cfae7618b2c9dc
Justas1988 <b.justas@outlook.com>
2021-10-05 17:16:05 +0300

    edit module border color now matches card's

--- src/Components/EditModule.jsx
+++ src/Components/EditModule.jsx
@@ -1,9 +1,18 @@
 import React, { useState, useEffect, useCallback } from 'react';
 
+
 export default function EditModule({ open, editCardHandler, cardsArray }) {    
     const [titleText, setEditorTitleText] = useState("New card Title") //input state
     const [mainText, setEditorMainText] = useState("New Main card Text") //input state
     const [bottomText, setEditorBottomText] = useState("New Bottom card Text") //input state  
+    const [borderColor, setBorderColor] = useState("transparent") //edit window border color state
+
+    
+    
+    function borderColorSetter(color) {        
+        setBorderColor(color)        
+    }
+
     
     const primaryState = useCallback( () => {
         const cardsCopy = [...cardsArray]        
@@ -15,12 +24,13 @@ export default function EditModule({ open, editCardHandler, cardsArray }) {
             setEditorTitleText(newTitle)
             setEditorMainText(newMain)
             setEditorBottomText(newBottom)
+            borderColorSetter(cardsCopy[i].color)
         break;
             }
         }
     }, [cardsArray, open])
 
-    useEffect(  () => { primaryState(); }, [primaryState, open] );
+    useEffect(  () => { primaryState(); }, [primaryState, open] );    
 
     function titleInputHandler (e) {
         setEditorTitleText(e.target.value)
@@ -36,7 +46,7 @@ export default function EditModule({ open, editCardHandler, cardsArray }) {
         return null;
     }   
     return (
-        <div className="editModuleContainer">
+        <div className="editModuleContainer" style={{borderColor}}>
             <div className="editModule">                
                 <input className="editTitleInput" type="text" value={titleText} onChange={titleInputHandler} maxLength="17"/>                
                 <textarea className="editMainInput" type="text" value={mainText} onChange={mainInputHandler} maxLength="170"/>
